5ed76020f2f758b672a61028c2476d80520842e5
David Harris <davidharris006@gmail.com>
2019-05-25 23:39:29 -0700

    got form working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -14,13 +14,32 @@ module.exports = function (app) {
 
     app.post("/api/friends", function (req, res) {
         console.log(req.body);
+ const friend = {};
+  friend.Name = req.body.Name;
+  friend.Photo = req.body.photo;
+  const scores = [];
+        for (const key in req.body) {
+            
+            if (req.body.hasOwnProperty(key)) {
+                const element = req.body[key];
+                const numbers = parseFloat(element.charAt(0))
 
+                if (element === "Please select value"){
+                    return res.render("/survey")
+                }
+                else if (typeof numbers === "integer"){
+                    scores.push(numbers)
+                }
 
+                
+                console.log(scores + friend);
+            }
+        }
 
         // newfriend.routeName = newfriend.Name.replace(/\s+/g, "").toLowerCase();
 
 
-        return res.json(friendsInfo)
+        return res.render("/survey")
     });
     app.get("/api/friends", function (req, res) {
         return res.json(friendsInfo);
